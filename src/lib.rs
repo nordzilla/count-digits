bea84822e440541d7456f63b2aee664e5ed29ba2
Erik Nordin <nordzillacode@gmail.com>
2024-01-22 21:09:00 -0600

    Add the CountDigits trait definition

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,9 @@
 //! # CountDigits
 //!
 //! A trait to count the decimal digits of an integer.
+
+/// Count the decimal digits of an integer.
+pub trait CountDigits: Copy + Sized {
+    /// Returns the count of decimal digits in an integer.
+    fn count_digits(self) -> u32;
+}
