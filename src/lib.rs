6453d789f16036da0b8d90c273485f82e6202b43
Erik Nordin <nordzillacode@gmail.com>
2024-01-26 00:22:59 -0600

    Update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -58,8 +58,8 @@
 //! ```rust
 //! # use count_digits::CountDigits;
 //! assert_eq!(
-//!     0xF00D_i32.count_digits(),
-//!     0xF00D_i32.wrapping_neg().count_digits(),
+//!     867_5309_i32.count_digits(),
+//!     867_5309_i32.wrapping_neg().count_digits(),
 //! );
 //! ````
 //!
